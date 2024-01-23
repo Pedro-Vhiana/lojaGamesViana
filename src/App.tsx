f8d7149722a4064cd397b3e718f2496caaacfabe
Pedro-Vhiana <pedrohb.0222@gmail.com>
2024-01-23 16:39:28 -0300

    Criação CRUD categoria, Service, Models, e Rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
 import './App.css';
-import Home from './pages/home/Home';
+
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
+import ListarCategorias from './components/categorias/listarCategorias/ListarCategorias';
+import FormularioCategorias from './components/categorias/formularioCategorias/FormularioCategorias';
+import DeletarCategoria from './components/categorias/deletarCategoria/DeletarCategoria';
+
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Home/>
-    <Footer/>
+        <BrowserRouter>
+          <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/home" element={<Home />} />
+              <Route path="/categorias" element={<ListarCategorias />} />
+              <Route path="/cadastrarCategorias" element={<FormularioCategorias />} />
+              <Route path="/atualizarCategorias/:id" element={<FormularioCategorias />} />
+              <Route path="/deletarCategoria/:id" element={<DeletarCategoria />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
     </>
-);
+  );
 }
 export default App;
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import homeLogo from '../../assets/icons8-circle-64 (1).png'
+import { Link } from 'react-router-dom'
 
 
 function Navbar() {
@@ -11,12 +12,12 @@ function Navbar() {
      <div className='w-full bg-slate-900 text-white flex justify-center py-4'>
           <div className="container flex justify-between text-lg ">
             
-            <div className='text-2xl text-[#fff] font-bold uppercase flex items-center  '><img src={homeLogo} alt="" />Games Store V </div>
+            <Link to='/home' className='text-2xl text-[#fff] font-bold uppercase flex items-center  '><img src={homeLogo} alt="" />Games Store V </Link>
 
             <div className='flex gap-4 items-center uppercase'>
-              <div className='hover:underline'>Home</div>
-              <div className='hover:underline'>Categorias</div>
-              <div className='hover:underline'>Sair</div>
+              <Link to='/home' className='hover:underline'>Home</Link>
+              <Link to='/categorias' className='hover:underline'>Categorias</Link>
+              <Link to='/cadastrarCategorias' className='hover:underline'>Cadastrar Categoria</Link>
             </div>
           </div>
         </div>
